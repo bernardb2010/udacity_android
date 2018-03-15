f34f7a9edc93e95193ca8488726b31be19858cb7
Brian <b.bernard@dataltd.com>
2018-03-15 10:00:42 -0400

    renaming variables, adding image view desription

--- FootballScoreKeeper/app/src/main/java/com/example/bernardb2010/footballscorekeeper/MainActivity.java
+++ FootballScoreKeeper/app/src/main/java/com/example/bernardb2010/footballscorekeeper/MainActivity.java
@@ -7,78 +7,78 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int teamA = 0;
-    private final int teamB = 1;
-    private final int bothTeams = 2;
+    private final int TEAM_A = 0;
+    private final int TEAM_B = 1;
+    private final int BOTH_TEAMS = 2;
 
     private int teamAScore = 0;
     private int teamBScore = 0;
-    private TextView teamATextView;
-    private TextView teamBTextView;
+    private TextView teamAScoreBoard;
+    private TextView teamBScoreBoard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        teamATextView = findViewById(R.id.team_a_score);
-        teamBTextView = findViewById(R.id.team_b_score);
+        teamAScoreBoard = findViewById(R.id.team_a_score);
+        teamBScoreBoard = findViewById(R.id.team_b_score);
     }
 
     public void updateScore(View v) {
 
-        int teamToUpdate = -1;
+        int scoreboardToUpdate = -1;
         switch (v.getId()) {
             case R.id.tdButtonA:
                 teamAScore += 6;
-                teamToUpdate = teamA;
+                scoreboardToUpdate = TEAM_A;
                 break;
             case R.id.fgButtonA:
                 teamAScore += 3;
-                teamToUpdate = teamA;
+                scoreboardToUpdate = TEAM_A;
                 break;
             case R.id.extraPnTButtonA:
                 teamAScore += 1;
-                teamToUpdate = teamA;
+                scoreboardToUpdate = TEAM_A;
                 break;
             case R.id.twoPtConvButtonA:
                 teamAScore += 2;
-                teamToUpdate = teamA;
+                scoreboardToUpdate = TEAM_A;
                 break;
             case R.id.tdButtonB:
                 teamBScore += 6;
-                teamToUpdate = teamB;
+                scoreboardToUpdate = TEAM_B;
                 break;
             case R.id.fgButtonB:
                 teamBScore += 3;
-                teamToUpdate = teamB;
+                scoreboardToUpdate = TEAM_B;
                 break;
             case R.id.extraPnTButtonB:
                 teamBScore += 1;
-                teamToUpdate = teamB;
+                scoreboardToUpdate = TEAM_B;
                 break;
             case R.id.twoPtConvButtonB:
                 teamBScore += 2;
-                teamToUpdate = teamB;
+                scoreboardToUpdate = TEAM_B;
                 break;
             case R.id.resetButton:
                 teamAScore = 0;
                 teamBScore = 0;
-                teamToUpdate = bothTeams;
+                scoreboardToUpdate = BOTH_TEAMS;
         }
-        displayScore(teamToUpdate);
+        updateScoreBoard(scoreboardToUpdate);
     }
 
-    private void displayScore(int teamToUpdate) {
+    private void updateScoreBoard(int teamToUpdate) {
         switch (teamToUpdate) {
-            case teamA:
-                teamATextView.setText(String.valueOf(teamAScore));
+            case TEAM_A:
+                teamAScoreBoard.setText(String.valueOf(teamAScore));
                 break;
-            case teamB:
-                teamBTextView.setText(String.valueOf(teamBScore));
+            case TEAM_B:
+                teamBScoreBoard.setText(String.valueOf(teamBScore));
                 break;
-            case bothTeams:
-                teamATextView.setText(String.valueOf(teamAScore));
-                teamBTextView.setText(String.valueOf(teamBScore));
+            case BOTH_TEAMS:
+                teamAScoreBoard.setText(String.valueOf(teamAScore));
+                teamBScoreBoard.setText(String.valueOf(teamBScore));
                 break;
         }
     }
